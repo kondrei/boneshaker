9a14973cfd2de78c61f111636b88411becbb12d0
kondrei <kondrei@gmail.com>
2022-04-09 21:44:00 +0300

    menu implementation

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,11 @@
 export default function Navbar() {
     return (
-        <div>navbar</div>
+        <div>navbar
+            <div class="flex space-x-2 justify-center">
+                <p> Link 1 </p>
+                <p> Link 2 </p>
+                <p> Link 3 </p>
+            </div>
+        </div>
     )
 }
--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function my404() {
     return (
-        <div className="min-h-screen flex justify-center items-center flex-col" >
+        <div className="h-full flex justify-center items-center flex-col" >
             <p className="text-red-500 text-center text-4xl">nu am gasit nimic</p>
             <Link href="/">
                 <a className="block mt-10">Pagina principala</a>
